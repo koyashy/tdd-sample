d03c2fa2a03db1f290383bf3f3c06c8f7a4b9e7f
Akira Koyasu <mail@akirakoyasu.net>
2014-01-02 17:38:13 +0900

    add form

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
-/**
- * index.php
- *
- * @category App
- * @package  Pass.auone.jp
- * @author   Akira Koyasu <koyasu@mediba.jp>
- * @license  mediba inc. http://www.mediba.jp
- * @link     none
- */
 
-$template = file_get_contents(__DIR__  . '/' . basename(__FILE__, 'php') . 'html');
+$template = file_get_contents(__DIR__  . '/' . basename(__FILE__, '.php') . '.html');
 
 echo $template;
