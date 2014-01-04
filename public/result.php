c654121e8cfa37d156c1336235e19a9c421f54a2
Akira Koyasu <mail@akirakoyasu.net>
2014-01-04 17:30:07 +0900

    display dynamic result

--- public/result.php
+++ public/result.php
@@ -1,5 +1,12 @@
 <?php
+require_once __DIR__ . '/Calculator.php';
 require_once __DIR__ . '/View.php';
 
+$a = $_GET['a'];
+$b = $_GET['b'];
+
+$calculator = new Calculator();
+$result = $calculator->add($a, $b);
+
 $c = new View(__FILE__);
-$c->render();
+$c->render(compact('result'));
